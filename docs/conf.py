00ab81c72c022a185fb73e95685942032f7d4f38
Chris Preager <chris@qophii.com>
2018-05-14 15:09:22 +0100

    fix autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -23,8 +23,8 @@ from recommonmark.transform import AutoStructify
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- General configuration ------------------------------------------------
