e97f9f161ecef445f2271febb201439f0ced01f4
Chris Preager <chris@qophii.com>
2018-05-15 00:08:27 +0100

    Maths Sphinx syntax

--- chris_pipeline/analysis.py
+++ chris_pipeline/analysis.py
@@ -80,6 +80,33 @@ class AllSectionsExternal(ExternalTask):
 
             Project logo at ``_static/logo.*``.
 
+        Inline maths: :math:`a^2 + b^2 = c^2`.
+
+        Maths sections:
+
+        .. math::
+
+           (a + b)^2  &=  (a + b)(a + b) \\
+                      &=  a^2 + 2ab + b^2
+
+        Prevent wrapping:
+
+        .. math::
+           :nowrap:
+
+           \begin{eqnarray}
+              y    & = & ax^2 + bx + c \\
+              f(x) & = & x^2 + 2xy + y^2
+           \end{eqnarray}
+
+        Label for cross-referencing:
+
+        .. math:: e^{i\pi} + 1 = 0
+           :label: euler
+
+        Euler's identity, equation :eq:`euler`, was elected one of the most
+        beautiful mathematical formulas.
+
         :return: :class:`ChrisAggWeighted`
 
         .. [ref01] https://www.example.com
