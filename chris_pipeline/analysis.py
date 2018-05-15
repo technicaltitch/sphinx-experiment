944a2d08567250aeaff83d4c6b448a3a5a78ea00
Chris Preager <chris@qophii.com>
2018-05-15 02:19:24 +0100

    Fix maths syntax

--- chris_pipeline/analysis.py
+++ chris_pipeline/analysis.py
@@ -82,24 +82,7 @@ class AllSectionsExternal(ExternalTask):
 
         Inline maths: :math:`a^2 + b^2 = c^2`.
 
-        Maths sections:
-
-        .. math::
-
-           (a + b)^2  &=  (a + b)(a + b)
-                      &=  a^2 + 2ab + b^2
-
-        Prevent wrapping:
-
-        .. math::
-           :nowrap:
-
-           \begin{eqnarray}
-              y    & = & ax^2 + bx + c \
-              f(x) & = & x^2 + 2xy + y^2
-           \end{eqnarray}
-
-        Label for cross-referencing:
+        Maths sections, labelled for cross-referencing:
 
         .. math:: e^{i\pi} + 1 = 0
            :label: euler
